017eb9c51b49dff9ef9faaccc6860e8c91a7ed0a
Matteo <109825733+M-A-T-T-E-O@users.noreply.github.com>
2023-10-26 14:22:16 +0200

    Update validationNN.py

--- validationNN.py
+++ validationNN.py
@@ -13,10 +13,10 @@ def validationNN(MyNN, x_test, y_test):
  y = torch.reshape(x_test[:,1],(x_test.size(0),1))
  w = torch.reshape(x_test[:,2],(x_test.size(0),1))
 
- # Approximated NN's output u(x_test)
+ # Approximated u_t(x_test)
  u = MyNN(x_test[:,:2])*x*y*(1-x)*(1-y) + 0
 
- # Calculate l2-norm of the error, error = ( u(x,y) - u_t(x,y) ) * (w(x,y))^(1/2) for each (x,y) in x_test
+ # Calculate l2-norm of the error, error = ( u(x,y) - u_t(x,y) ) * (w(x,y))^(1/2), summing over the all (x,y) in x_test
  error = (y_test - u.detach().numpy())*np.sqrt(w)
  l2_error = np.linalg.norm(error)
 
@@ -27,8 +27,8 @@ def validationNN(MyNN, x_test, y_test):
  ax.set_xlabel("x", fontsize=16)
  ax.set_ylabel("y", fontsize=16)
  ax.set_zlabel("z", fontsize=16)
- ax.scatter(x_test[:, 0], x_test[:, 1], y_test, color='red',label='u(x,y) [ideal]',alpha=0.1)
- ax.scatter(x_test[:, 0], x_test[:, 1], u.detach().numpy(), color='green',label='u(x,y) [approximated]', alpha =0.1)
+ ax.scatter(x_test[:, 0], x_test[:, 1], y_test, color='red',label='u(x_test) [ideal]',alpha=0.1)
+ ax.scatter(x_test[:, 0], x_test[:, 1], u.detach().numpy(), color='green',label='u(x_test) [approximation]', alpha =0.1)
  plt.legend()
  plt.show()
 
@@ -43,7 +43,7 @@ def validationNN(MyNN, x_test, y_test):
  plt.show()
 
  # Show both the target and the output from the trained NN
- print("\nThe output of the NN (predictions) is:\n", u, "\n\n",
+ print("\nThe output from the NN (predictions) is:\n", u, "\n\n",
        'The target output (from measurements) is:',
        "\n", y_test, "\n\n",
        'The l2-norm of the error is approximately equal to', l2_error)
